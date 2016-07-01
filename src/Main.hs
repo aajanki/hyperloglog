882eb55645e5346d645d0ae08d751aad0c9c69f5
Antti Ajanki <antti.ajanki@iki.fi>
2016-07-01 16:16:31 +0300

    Use 14 bits for registers
    as recommended by the Google paper

--- src/Main.hs
+++ src/Main.hs
@@ -7,11 +7,11 @@ import Data.List
 main :: IO ()
 main = do putStrLn $ intercalate "\n" (map (show . count) hlls)
   where
-    xs = take 20000 strings
+    xs = take 60000 strings
     hlls = scanl append hllCounter xs
 
 hllCounter :: HLLCounter String
-hllCounter = emptyHLL 12 (asWord64 . Data.Hash.hash)
+hllCounter = emptyHLL 14 (asWord64 . Data.Hash.hash)
 
 strings :: [String]
 strings = [x ++ [a] | x <- "" : strings, a <- ['a'..'z']]
